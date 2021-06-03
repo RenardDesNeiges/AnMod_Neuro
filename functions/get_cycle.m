b1a28a07d4c1cd74d788eb57561b3d3070f84e1a
Titouan Renard <titouan.renard@gmail.com>
2021-06-03 15:49:29 +0200

    cycle detection

---
+++ functions/get_cycle.m
@@ -0,0 +1,27 @@
+function [cycle,cycle_index,cycle_time] = get_cycle(ankle,hip,sr)
+%GET_CYCLE_LEN Summary of this function goes here
+%   Detailed explanation goes here
+    normalized_y_ankle = ankle-hip;
+    normalized_y_ankle_speed = diff(normalized_y_ankle);
+    
+    %filter the  finite differences vector to remove noise
+    Low = 7/(sr/2);
+    [b1, a1] = butter(2,Low,'low'); 
+    normalized_y_ankle_speed_flt = filter(b1, a1, normalized_y_ankle_speed);
+    
+    zci = @(v) find(v(:).*circshift(v(:), [-1 0]) <= 0);
+
+    zeros_ankle = (zci(normalized_y_ankle));
+    zeros_ankle = zeros_ankle(1:end-1);
+    
+    derivative_when_zero = normalized_y_ankle_speed_flt(zeros_ankle);
+    cycle_index = zeros_ankle( derivative_when_zero < 0);
+    cycle = (1/sr) * cycle_index;
+    
+    cycle_time = zeros(size(cycle)-1);
+    
+    for i = 2:1:(size(cycle)-1)
+        cycle_time(i) = cycle(i) - cycle(i-1);
+    end
+end
+
